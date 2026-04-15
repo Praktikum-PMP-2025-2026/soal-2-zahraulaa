e2fafc5abc2790ac513512009e849fe1657f4d7a
Zahra Aulia <13224007@mahasiswa.itb.ac.id>
2026-04-15 09:50:14 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -1,34 +1,29 @@
 #include <stdio.h>
 
-int transformasi(int i){
-    if(i <= 1){
-        return 1;
+int count(int n) {
+    if (n == 1) {
+        return 0;
     }
-    return i * transformasi(i - 1);
+    if (n%2 == 0) {
+        return 1 + count(n/2);
+    } 
+    
+    else if (n%3 == 0 && n>1) {
+        return 1 + count((3*n)+1);
+    }
+
+    else {
+       return 0;
+    }
 }
 
-int hitung() {
+int main() {
     int T, n;
-    scanf("%d %d", &T, &n);
+    scanf("%d", &T);
 
-    for (int i = n; i <= 1; i++) {
-        if (n % 2 == 0) {
-            n/2;
-        } else if (n % 3 == 0 && n>1) {
-            ((3*n)+1);
-        } 
+    for (int i = 0; i < T; i++) {
+        scanf("%d", &n);
+        printf("LANGKAH %d\n", count(n));
     }
-    
-    int main() {
-    int count = 0; 
-    int cek[1000] = {0}; 
-    count(transformasi);
-           cek[n] = 1; 
-        n = hitung(n); 
-        count++; 
-    } 
-
-    printf("LANGKAH %d \n", count);
     return 0;
-
 }
