103ac1a38f773403c063cf9b28b9f4f647ed2b35
Zahra Aulia <13224007@mahasiswa.itb.ac.id>
2026-04-15 09:29:12 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 
-int main(void) {
+int transformasi(int i){
+
+    if(i <= 1){
+        return 1;
+    }
+    return i * transformasi(i - 1);
+}
+
+int main() {
+    int T, n;
+    scanf("%d %d", &T, &n);
+
+    for (int i = n; i <= 1; i++) {
+        if (n % 2 == 0) {
+            n/2;
+        } else if (n % 3 == 0 && n>1) {
+            ((3*n)+1);
+        } 
+    }
+   
+    printf("LANGKAH %d \n", n);
+
     return 0;
+
 }
