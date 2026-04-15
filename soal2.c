ee148f191f21998a337cb7cc905d10e41841d5b0
Zahra Aulia <13224007@mahasiswa.itb.ac.id>
2026-04-15 11:00:56 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 
 int count(int n) {
+    // printf("%d\n", n);
     if (n == 1) {
         return 0;
     }
-    else{
-        if (n%2 == 0) {
-            return 1 + count(n/2);
-        } 
+    if (n%2 == 0) {
+        return 1 + count(n/2);
+    } 
     
-        else {
-            return 1 + count((3*n)+1);
-        }
+    else {
+        return 1 + count((3*n)+1);
     }
 }
 
 int main() {
     int T;
 
-    scanf("%d", &T) != 1;
+    scanf("%d", &T);
 
     for (int i = 0; i < T; i++) {
         int n;
-        int langkah = count(n);
         scanf("%d", &n);
+        int langkah = count(n);
         printf("LANGKAH %d\n", langkah);
     }
     return 0;
